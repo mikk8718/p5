7b57c88453aff30b90c066ca9fa71020beb55d02
Mkth <56339472+Mkth@users.noreply.github.com>
2020-11-26 13:25:00 +0100

    CGP Mini-project Simple example
    This is a very simple two-piece code for a Compute shader and a script for a simple program demonstrating how to use a compute shader to make a green texture.

---
+++ ShaderScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderScript : MonoBehaviour{
+    public ComputeShader compute;
+    public RenderTexture result;
+    int kernel;
+    void Start(){
+        result = new RenderTexture(256,256,32);
+        result.enableRandomWrite = true;
+        result.Create();
+        kernel = compute.FindKernel("CSMain");
+    }
+
+    void Update(){
+        compute.SetTexture(kernel, "Result", result);
+        compute.Dispatch(kernel,32,32,1);
+    }
+}
